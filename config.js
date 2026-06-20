e8d74deea6910e4a25a2223287aa71a76866281e
affilinno <affilinno@gmail.com>
2026-06-20 12:14:51 +0900

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbxP_DDLcxEkZfH03Y-448UaA1NjxdH5sIS8zw0Td9oWEY2M91AXRtV5QXjS4gyOU6lJFw/exec';
+const GAS_API_URL = 'https://script.google.com/macros/s/AKfycbx8_DssKpcofkq8_LvkG9coXsmmKeCpL6lG7taRiJClVObzG97LUPO9AmQcJnU5so-ULw/exec';
